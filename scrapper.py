b0c73a3ccc23321d4632fd66904d3a1a2ad764c0
Marcus Danilo <mdanilor@gmail.com>
2017-12-07 16:47:27 -0200

    Estimadíssimo!

--- scrapper.py
+++ scrapper.py
@@ -16,10 +16,11 @@ class Botnet:
         self.tor = False
         self.ports = []
         self.country = None
-        self.server = None
+        self.webServer = None
         self.so = None
 
-    def setOnlineStatus(self):
+    # Obtain server status
+    def updateOnlineStatus(self):
         try:
             r = requests.get("http://" + self.url, headers = headers)
             if r.status_code == 200:
@@ -30,38 +31,47 @@ class Botnet:
         except requests.exceptions.ConnectionError:
             self.online = False
 
-    def setServer(self):
+
+    # Obtains web server
+    def updateWebServer(self):
         try:
             r = requests.get("http://" + self.url, headers = headers)
             if r.headers['server'] != None:
-                self.server = r.headers['server']
+                self.webServer = r.headers['server']
 
         except:
             return
 
-    def setCountry(self):
+    # Obtains country
+    def checkCountry(self):
         u = self.url.find('/')
         p = subprocess.Popen(["geoiplookup", self.url[:u]], stdout=subprocess.PIPE)
         output, err = p.communicate()
         self.country = output.decode(encoding='UTF-8')[23:25]
 
-    def getOpenPorts(self):
+
+    def checkOpenPorts(self):
         nm = nmap.PortScanner()
         u = self.url.find('/')
         host = self.url[:u]
         nm.scan(host, '1-1000')
-        self.ports = nm[nm.all_hosts()[0]]['tcp'].keys()
+        self.ports = list(nm[nm.all_hosts()[0]]['tcp'].keys())
 
-    def setInfo(self):
-        self.setOnlineStatus()
-        self.setCountry()
-        self.setServer()
-        self.getOpenPorts()
+    # Returns True if the C&C server is found online and False otherwise.
+    def updateInfo(self):
+        self.updateOnlineStatus()
+        if self.online:
+            self.checkCountry()
+            self.updateWebServer()
+            self.checkOpenPorts()
+            return True
+        else:
+            return False
 
-    def getInfo(self):
-        return self.url + ", " + str(self.online) + ", " + str(self.country) + ", " + str(self.server)
+    def getCsvData(self):
+        return self.url + "; " + str(self.online) + "; " + str(self.country) + "; " + str(self.webServer) + "; " + str(self.ports)
 
-r = requests.get('http://cybercrime-tracker.net/index.php?s=0&m=100').text
+r = requests.get('http://cybercrime-tracker.net/index.php?s=0&m=2').text
 
 soup = BeautifulSoup(r, 'html.parser')
 
@@ -78,5 +88,5 @@ for server in botnets_list:
     botnets.append(Botnet(bot_info[0], bot_info[1], bot_info[2], bot_info[3]))
 
 for bot in botnets:
-    bot.setInfo()
-    bot.getInfo()
+    if bot.updateInfo():
+        print (bot.getCsvData())
