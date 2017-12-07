ac88905452ccb3f917a7dfb9fcb8396da31d249b
André Almeida <fda.andre@gmail.com>
2017-12-07 16:50:30 -0200

    Add main function

--- scrapper.py
+++ scrapper.py
@@ -40,8 +40,8 @@ class Botnet:
             return
 
     def setCountry(self):
-        u = self.url.find('/')
-        p = subprocess.Popen(["geoiplookup", self.url[:u]], stdout=subprocess.PIPE)
+        path = self.url.find('/')
+        p = subprocess.Popen(["geoiplookup", self.url[:path]], stdout=subprocess.PIPE)
         output, err = p.communicate()
         self.country = output.decode(encoding='UTF-8')[23:25]
 
@@ -61,22 +61,26 @@ class Botnet:
     def getInfo(self):
         return self.url + ", " + str(self.online) + ", " + str(self.country) + ", " + str(self.server)
 
-r = requests.get('http://cybercrime-tracker.net/index.php?s=0&m=100').text
+def main(argv):
+    r = requests.get('http://cybercrime-tracker.net/index.php?s=0&m=100').text
 
-soup = BeautifulSoup(r, 'html.parser')
+    soup = BeautifulSoup(r, 'html.parser')
 
-botnets_list = soup.find('tbody').find_all('tr')
+    botnets_list = soup.find('tbody').find_all('tr')
 
-botnets = []
+    botnets = []
 
-for server in botnets_list:
-    data = server.find_all('td')
-    bot_info = []
-    for d in data:
-        bot_info.append(d.text)
+    for server in botnets_list:
+        data = server.find_all('td')
+        bot_info = []
+        for d in data:
+            bot_info.append(d.text)
 
-    botnets.append(Botnet(bot_info[0], bot_info[1], bot_info[2], bot_info[3]))
+        botnets.append(Botnet(bot_info[0], bot_info[1], bot_info[2], bot_info[3]))
 
-for bot in botnets:
-    bot.setInfo()
-    bot.getInfo()
+    for bot in botnets:
+        bot.setInfo()
+        bot.getInfo()
+
+if __name__ == "__main__":
+    main(sys.argv)
